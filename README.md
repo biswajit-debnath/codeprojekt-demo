cad7a8c9480f2767c2c88dcac50458a98ae3238e
biswajit-debnath <biswajitdebnath405@gmail.com>
2025-02-25 12:35:18 +0530

    Demo: Pushed commign soon page as demo

---
+++ README.md
@@ -0,0 +1,8 @@
+# CodeProjekt
+
+## Current Flow:
+* Currently base structure is in implementation testing phase.
+* Please checkout to ```structureTesting``` branch for current live project.
+* For any new changes
+    * Create a branch from the above branch
+    * Make a pull request to the above branch with appropriate description.
---
+++ script.js
@@ -0,0 +1,23 @@
+// Set the countdown date and time
+const countDownDate = new Date("July 5, 2025 15:37:25").getTime(); 
+
+const x = setInterval(function() {
+
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
